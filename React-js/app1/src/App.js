9ad6a4200a908abcc260a7c68aa5ddc6974d8c4f
vijayAV55 <vijayram964932@gmail.com>
2024-05-24 19:57:17 +0530

    add

---
+++ React-js/app1/src/App.js
@@ -0,0 +1,49 @@
+
+import { BlogPostManage } from "./Assignments-1/BlogPostManager"
+import { CalendarAppointments } from "./Assignments-1/CalendarAppointments"
+import { ChatApplication } from "./Assignments-1/ChatApplication"
+import { EcommerceCart } from "./Assignments-1/EcommerceCart"
+import { EmployeeTimeTracker } from "./Assignments-1/EmployeeTimeTracker"
+import { FitnessChallengeTracke } from "./Assignments-1/FitnessChallengeTracke"
+import { FlightBookingSystem } from "./Assignments-1/FlightBookingSystem"
+import { GameCharacter } from "./Assignments-1/GameCharacter"
+import { HotelReservationSystem } from "./Assignments-1/HotelReservationSystem"
+import { InventoryManagementSystem } from "./Assignments-1/InventoryManagementSystem"
+import { LanguageLearningApp } from "./Assignments-1/LanguageLearningApp"
+import { MovieDatabase } from "./Assignments-1/MovieDatabase"
+import { MusicBandManager } from "./Assignments-1/MusicBandManager"
+import { MusicPlayer } from "./Assignments-1/MusicPlayer"
+import { ProjectManagementTool } from "./Assignments-1/ProjectManagementTool"
+import { RealEstateListings } from "./Assignments-1/RealEstateListings"
+import { RecipeBook } from "./Assignments-1/RecipeBook"
+import { RecipeGenerator } from "./Assignments-1/RecipeGenerator"
+import { StudentEnrollmentSystem } from "./Assignments-1/StudentEnrollmentSystem"
+import { TaskTracker } from "./Assignments-1/TaskTracker"
+import { WeatherApp } from "./Assignments-1/WeatherApp"
+
+function App() {
+  return <div>
+    <StudentEnrollmentSystem/>
+    <MusicPlayer/>
+    <RecipeBook/>
+    <MovieDatabase/>
+    <ChatApplication/>
+    <InventoryManagementSystem/>
+    <CalendarAppointments/>
+    <GameCharacter/>
+    <EcommerceCart/>
+    <LanguageLearningApp/>
+    <TaskTracker/>
+    <WeatherApp/>
+    <HotelReservationSystem/>
+    <EmployeeTimeTracker/>
+    <RecipeGenerator/>
+    <FlightBookingSystem/>
+    <MusicBandManager/>
+    <BlogPostManage/>
+    <FitnessChallengeTracke/>
+    <RealEstateListings/>
+    <ProjectManagementTool/>
+  </div>
+}
+export default App
