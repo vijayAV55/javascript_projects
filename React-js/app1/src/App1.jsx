b9f41cb6107f503a41407a36ec63923567a46897
vijayAV55 <vijayram964932@gmail.com>
2024-05-25 21:48:57 +0530

    add

--- React-js/app1/src/App1.jsx
+++ React-js/app1/src/App1.jsx
@@ -2,12 +2,14 @@ import {
   BlogPostManage,
   BlogPostManager,
 } from "./Assignment-class-components/BlogPostManager";
+import { CalendarAppointments } from "./Assignment-class-components/CalendarAppointments";
 
 function App1() {
   return (
     <div>
       <h1>vijayram</h1>
       <BlogPostManager />
+      <CalendarAppointments />
     </div>
   );
 }
--- React-js/app1/src/Assignment-class-components/CalendarAppointments.jsx
+++ React-js/app1/src/Assignment-class-components/CalendarAppointments.jsx
@@ -1,24 +1,58 @@
-import { useState } from "react";
+import { Component } from "react";
 
-export function CalendarAppointments() {
-  const [CalendarAppointments] = useState({
-    eventname: "therrtham",
-    date: "23/4/2026",
-    time: "9:30 am",
-    location: "Bank",
+export class CalendarAppointments extends Component {
+  state = {
+    name: "ram",
+    person: {
+      eventname: "therrtham",
+      date: "23/4/2026",
+      time: "9:30 am",
+      location: "Bank",
+    },
+    student: ["guna", "vijay", "ram", "kumar"],
+    Newposts: ["4,4,4"],
+  };
+  newappointments = () => {
+    const newstate = { ...this.state };
+    newstate.student.push("ajay", "tarun");
+    this.setState(newstate);
+  };
 
-    newappointments: function () {},
-    updateappointments: function () {},
-    deleteappointments: function () {},
-  });
+  updateappointments = () => {
+    const newstate = { ...this.state };
+    newstate.student.splice(1, 0, "venky");
+    this.setState(newstate);
+  };
+  deleteappointments = () => {
+    const newstate = { ...this.state };
+    newstate.student.pop();
+    this.setState(newstate);
+  };
 
-  return (
-    <div>
-      <ul>
-        {Object.keys(CalendarAppointments).map((key) => {
-          return <li>{CalendarAppointments[key]}</li>;
-        })}
-      </ul>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <button onClick={this.newappointments}>newappointments</button> <br />
+        <br />
+        <button onClick={this.updateappointments}>updateappointments</button>
+        <br /> <br />
+        <button onClick={this.deleteappointments}> deleteappointments</button>
+        <br />
+        <br />
+        <ul>
+          {this.state.student.map((val) => (
+            <li>{val}</li>
+          ))}
+        </ul>
+        <ul>
+          <p>{this.state.person.eventname}</p>
+          <p>{this.state.person.date}</p>
+          <p>{this.state.person.time}</p>
+          <p>{this.state.person.location}</p>
+          <p>{this.state.posttitle}</p>
+          <p>{this.state.Newposts}</p>
+        </ul>
+      </div>
+    );
+  }
 }
