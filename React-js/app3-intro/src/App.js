06fcbab105139ca262c8f9a88549944c52ed790e
vijayAV55 <vijayram964932@gmail.com>
2024-06-08 10:06:58 +0530

    add

---
+++ React-js/app3-intro/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import { Sample1 } from "./Assignment-1/Sample_1";
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>vijayram</h1>
+        <Sample1 />
+      </div>
+    );
+  }
+}
