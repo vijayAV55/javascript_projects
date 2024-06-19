a60cbe525dc73683673b2c0a8f84f8a884c198fb
vijayAV55 <vijayram964932@gmail.com>
2024-06-19 10:10:50 +0530

    Add

--- React-js/app2-intro/src/App.js
+++ React-js/app2-intro/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Boost_1 from "./Boostrap-Assignment/Boost_1";
 
 function App() {
   return (
     <div>
-      <h1>projects</h1>
+      <h1>Boostrap-Assignment</h1>
+      <Boost_1 />
     </div>
   );
 }
---
+++ React-js/app2-intro/src/Boostrap-Assignment/Boost_1.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { button } from "bootstrap.esm.min.js";
+export default class Boost_1 extends Component {
+  render() {
+    return (
+      <div>
+        <h2>vijayram</h2>
+        <button>button</button>
+      </div>
+    );
+  }
+}
