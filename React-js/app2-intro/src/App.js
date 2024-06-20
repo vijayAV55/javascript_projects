644aadcbb54d26469cb1790e1967f96c9bc71b1f
vijayAV55 <vijayram964932@gmail.com>
2024-06-20 16:34:02 +0530

    ADD

--- React-js/app2-intro/src/App.js
+++ React-js/app2-intro/src/App.js
@@ -1,17 +1,35 @@
 import "./App.css";
-import Boost_1 from "./Boostrap-Assignment/Boost_1";
-import Sample1 from "./Boostrap-Assignment/Sample1";
-import Sample2 from "./Boostrap-Assignment/Sample2";
-import Sample3 from "./Boostrap-Assignment/Sample3";
+// import Boost_1 from "./Boostrap-Assignment/Boost_1";
+// import Sample1 from "./Boostrap-Assignment/Sample1";
+// import Sample2 from "./Boostrap-Assignment/Sample2";
+// import Sample3 from "./Boostrap-Assignment/Sample3
+
+import Dineshform from "./CURD_Assignment/Dinesh_Form";
+import Himadeepform from "./CURD_Assignment/Himadeep_form";
+import Nethajiform from "./CURD_Assignment/Nethaji_Form";
+import Saiform from "./CURD_Assignment/Sai_Form";
+import Vijayform from "./CURD_Assignment/Vijay_Form";
 
 function App() {
   return (
     <div>
-      <h1>Boostrap-Assignment</h1>
+      {/* <h1>Boostrap-Assignment</h1>
       <Boost_1 /> <hr />
       <Sample1 /> <hr />
       <Sample2 />
-      <Sample3 />
+      <Sample3 /> */}
+      {/* <Sample4 /> */}
+      <Dineshform />
+      <hr />
+      <Vijayform />
+
+      <hr />
+      <Nethajiform />
+      <hr />
+      <Saiform />
+      <hr />
+      <Himadeepform />
+      <hr />
     </div>
   );
 }
