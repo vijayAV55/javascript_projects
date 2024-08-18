b9afe8afa6d9ddd5a9b0b182bd29d25070c23ed7
vijayAV55 <vijayram964932@gmail.com>
2024-08-18 13:11:47 +0530

    ADD comming soon

--- Reactjs-projecet2/my-app/src/App.js
+++ Reactjs-projecet2/my-app/src/App.js
@@ -1,6 +1,8 @@
 // import DigitalClock from "./DigitalClock/DigitalClock";
 
-import MiniCalendar from "./Mini-Calendar/MiniCalendar";
+import ComingSoon from "./Coming-Soon/ComingSoon";
+
+// import MiniCalendar from "./Mini-Calendar/MiniCalendar";
 
 // import QRCodeGenerator from "./QR-Code-Generator/QRCodeGenerator";
 
@@ -9,7 +11,8 @@ function App() {
     <div className="App">
       {/* <DigitalClock /> */}
       {/* <QRCodeGenerator /> */}
-      <MiniCalendar />
+      {/* <MiniCalendar /> */}
+      <ComingSoon />
     </div>
   );
 }
